5eb924343049e79c25e68efe2f5b14aa3593befd
sk <semaria.eyzac@gmail.com>
2019-02-06 22:35:57 -0800

    create App.jsx and import it in index.jsx

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function App(){
+  return(
+    <div>
+      <h1>HEllo PRetty</h1>
+    </div>
+  );
+}
--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 ReactDom.render(
-  <div>
-    <h1>Hello Pretty</h1>
-  </div>,
+  <App/>,
   document.getElementById('react-app-root')
 );

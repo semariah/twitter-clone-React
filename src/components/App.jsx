135bf60d5ef7d746b1ef9213577692c5fe0e15b3
sk <semaria.eyzac@gmail.com>
2019-02-11 14:35:49 -0800

    create the Bio.jsx file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from './Header';
 import DressList from './DressList';
+import Profile from './Profile';
+import Bio from './Bio';
 
 
 function App(){
   return(
     <div>
       <Header/>
+      <Profile/>
+      <Bio/>
       <DressList/>
     </div>
   );
---
+++ src/components/Bio.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Bio(){
+  return(
+    <div>
+      <h4>HEllo, My name is semhar. I am a webdeveloper and web designer. I have designed many websites as a freelance desiner. I have a biochemistry background but i wanted to be a part of the tech world that I started studing python on my own, from there, its history. I started and learned and worked as a webdeveloper with in a year gap. A very big accomplishmnet that is still on process towards my goals but happy I made this choice. I use JAvascript, React, Java, Angular JS, HTML & CSS.</h4>
+    </div>
+  )
+}
+
+export default Bio;
